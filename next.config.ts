6339ed1106146250f068247ccff46ea3d963eaa9
liyan585 <liyanmohamed007@gmail.com>
2026-04-04 18:19:26 +0530

    Remove basePath from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
+import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
-  basePath: "/wizardoftheducky",
   images: {
     unoptimized: true,
   },
 };
+
+export default nextConfig;
